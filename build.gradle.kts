bd249dc04301c09acbc97ecab26fb8e6fc4d0b43
mohammadmirdar <m.mirdar@igap.net>
2024-03-19 14:56:42 +0330

    add application dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,4 +2,6 @@
 plugins {
     alias(libs.plugins.androidApplication) apply false
     alias(libs.plugins.jetbrainsKotlinAndroid) apply false
+    id("com.google.dagger.hilt.android") version "2.48" apply false
+    id ("io.realm.kotlin") version "1.11.0" apply false
 }
